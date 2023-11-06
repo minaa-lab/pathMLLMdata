2e08706a0e78fe41bff89172a8852253cecdc030
Henry-cyber <839139251@qq.com>
2023-11-06 22:20:22 +0800

    Add classification

---
+++ classification/h5_convert_to_img.py
@@ -0,0 +1,20 @@
+5# 作者：韦芃臣
+# 2023年11月06日19时57分03秒
+import h5py
+from PIL import Image
+import numpy as np
+
+# 打开 .h5 文件
+h5_file = h5py.File(r'C:\LuoLab\pathMLLMdata\classification\PCam\camelyonpatch_level_2_split_train_x.h5', 'r')
+
+# 获取图像数据
+images_data = h5_file['x']  # 假设数据集名称为 "images"
+
+# 遍历前 500 行数据并保存为图像文件
+for i, img_data in enumerate(images_data[:5000]):
+    # 假设图像数据是二维数组
+    img = Image.fromarray(img_data, mode='RGB')  # 'L' 表示灰度图像
+
+    # 保存图像为 JPEG 文件（或者使用其他格式如 PNG）
+    img.save(rf'C:\LuoLab\pathMLLMdata\classification\PCam\images\output_image_{i}.jpg')
+
---
+++ classification/make_classification_chatml_PCam.py
@@ -0,0 +1,67 @@
+import h5py
+import json
+import os
+from PIL import Image
+
+# 指定 PNG 图像文件夹路径
+images_folder = 'C:\LuoLab\pathMLLMdata\classification\PCam\images'
+# 指定 HDF5 标签文件路径
+labels_file = 'C:\LuoLab\pathMLLMdata\classification\PCam\camelyonpatch_level_2_split_train_y.h5'
+
+# 定义查询列表
+query_list_cancer = ["Does this image contain cancerous cells?",
+                     "Can you identify cancer cells in this image?",
+                     "Please tell me if cancer is present in this image.",
+                     "Are there signs of cancer in this image?",
+                     "Could you please determine if there are cancer cells in this image?",
+                     "Is this image depicting malignant cells?"]
+
+query_list_normal = ["Describe the features in this image.",
+                     "Please provide details about this image."]
+
+def get_label_value(label_data):
+    # 提取有效的标签值，根据具体情况可能需要修改索引
+    return label_data[0][0][0]
+
+
+def make_chatml_data(images_folder, labels_file, caption_chatml_data):
+    # 列出图像文件夹中的所有 JPG 图像文件
+    image_files = [f for f in os.listdir(images_folder) if f.endswith('.jpg')]
+
+    # 打开 HDF5 标签文件
+    with h5py.File(labels_file, 'r') as labels_h5:
+        labels = labels_h5['y'][:5001]  # 假设 HDF5 数据集名称为 "labels"
+
+    for i, image_file in enumerate(image_files):
+        img_path = os.path.join(images_folder, image_file)
+        img_label = get_label_value(labels[i])  # 获取有效的标签值
+
+        # 创建用户查询，选择适当的查询列表
+        if img_label == 1:  # 包含癌症细胞
+            query = query_list_cancer[i % len(query_list_cancer)]
+
+        else:  # 正常图像
+            query = query_list_normal[i % len(query_list_normal)]
+
+        query = "<img>" + img_path + "</img> " + query
+        id = "image_" + str(i)
+
+        # 创建助手回复，这里可以根据需要自定义回复内容
+        if img_label == 1:
+            answer = "Yes, this image contains cancerous cells."
+        else:
+            answer = "No, there are no cancerous cells in this image."
+
+        caption_chatml_data.append(
+            {"id": id, "conversations": [{"from": "user", "value": query}, {"from": "assistant", "value": answer}]})
+
+
+if __name__ == '__main__':
+    caption_chatml_data = []
+    make_chatml_data(images_folder, labels_file, caption_chatml_data)
+
+    # 将 ChatML 数据保存为 JSON 文件
+    with open('C:\LuoLab\pathMLLMdata\classification\PCam\chatml_data.json', 'w') as json_file:
+        json.dump(caption_chatml_data, json_file)
+
+    print("ChatML data saved to chatml_data.json.")
